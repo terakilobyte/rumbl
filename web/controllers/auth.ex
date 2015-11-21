4b80620cb24f1c5d3c29dfdaa1e582d1a795c946
terakilobyte <terakilobyte@gmail.com>
2015-11-20 19:30:17 -0800

    Add user token for socket authentication

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -42,13 +42,25 @@ defmodule Rumbl.Auth do
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
     user = user_id && repo.get(Rumbl.User, user_id)
-    assign(conn, :current_user, user)
+    if user = user_id && repo.get(Rumbl.User, user_id) do
+      put_current_user(conn, user)
+    else
+      assign(conn, :current_user, nil)
+    end
   end
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 end
