7e01ba2d3b621307648e508ac2f11491760f836c
terakilobyte <terakilobyte@gmail.com>
2015-11-22 11:27:54 -0800

    Adds realtime lookup to video commenting

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -26,22 +26,39 @@ defmodule Rumbl.VideoChannel do
 
   def handle_in("new_annotation", params, user, socket) do
     changeset =
-      IO.inspect(user)
+    user
     |> build(:annotations, video_id: socket.assigns.video_id)
     |> Rumbl.Annotation.changeset(params)
 
     case Repo.insert(changeset) do
-      {:ok, annotation} ->
-        broadcast! socket, "new_annotation",
-        %{
-          user: Rumbl.UserView.render("user.json", %{user: user}),
-          body: annotation.body,
-          at: annotation.at
-        }
+      {:ok, ann} ->
+        broadcast_annotation(socket, ann)
+        Task.start_link(fn -> compute_additional_info(ann, socket) end)
         {:reply, :ok, socket}
 
       {:error, changeset} ->
         {:reply, {:error, %{errors: changeset}}, socket}
+    end
+  end
+
+  defp broadcast_annotation(socket, annotation) do
+    annotation = Repo.preload(annotation, :user)
+    render_ann = Phoenix.View.render(AnnotationView, "annotation.json",
+                                     %{annotation: annotation})
+    broadcast! socket, "new_annotation", rendered_ann
+  end
+
+  defp compute_additional_info(ann, socket) do
+    for result <- Rumbl.InfoSys.compute(ann.body, limit: 1) do
+      attrs = %{url: result.url, body: result.text, at: ann.at}
+      info_changeset = result.backend
+      |> build(:annotations, video_id: ann.video_id)
+      |> Rumbl.Annotation.changeset(attrs)
+
+      case Repo.insert(info_changeset) do
+        {:ok, info_ann} -> broadcast_annotation(socket, info_ann)
+        {:error, _changeset} -> :ignore
+      end
     end
   end
 end
